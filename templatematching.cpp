16aee49e2f2911d183280160bc065fb63a186d66
Youssef <therealyoussef@aucegypt.edu>
2016-11-24 20:21:02 +0200

    Created TemplateMatching Class

---
+++ templatematching.cpp
@@ -0,0 +1,6 @@
+#include "templatematching.h"
+
+TemplateMatching::TemplateMatching()
+{
+
+}
---
+++ templatematching.h
@@ -0,0 +1,12 @@
+#ifndef TEMPLATEMATCHING_H
+#define TEMPLATEMATCHING_H
+
+#include <opencv2/opencv.hpp>
+
+class TemplateMatching
+{
+public:
+    TemplateMatching();
+};
+
+#endif // TEMPLATEMATCHING_H
